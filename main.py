4321137777242371f9a1a3656a5af531734b0379
Pham Nguyen Phuong <24122011@student.hcmus.edu.vn>
2024-11-22 23:53:15 +0700

    tới 2.5

--- main.py
+++ main.py
@@ -22,8 +22,13 @@ from tensorflow.keras.preprocessing.image import load_img, img_to_array
     # trich anh ra
 import glob
 
+#     # thư mục chứa file DataSet
+# base_dir = os.path.dirname(__file__)
+
     # Sử dụng glob để tìm các tệp .xml trong đường dẫn cục bộ trên máy tính
-path = glob.glob(r'C:/Users/HP/Documents/ĐH/GT TTNT/Thực hành/project/GITHUB/Project_1/DataSet/images/*.xml')
+# path = glob.glob(os.path.join(base_dir, 'DataSet/images/*.xml'))
+
+path = glob.glob(r'C:/Users/HP/Documents/University/GT TTNT/Practice/project/GITHUB/Project_1/DataSet/images/*.xml')
 
     # Tạo dictionary
 labels_dict = dict(filepath=[],xmin=[],xmax=[],ymin=[],ymax=[])
@@ -49,14 +54,23 @@ df = pd.DataFrame(labels_dict)
 df.to_csv('labels.csv',index=False)
 print(df.head())
 
-    # Hàm lấy đường dẫn ảnh
+   # Hàm lấy đường dẫn ảnh
 filename = df['filepath'][0]
 def getFilename(filename):
     filename_image = xet.parse(filename).getroot().find('filename').text
-    filepath_image = os.path.join('DataSet/images',filename_image)
+    # filepath_image = os.path.join('DataSet/images',filename_image)
+    filepath_image = os.path.join('Documents/University/GT TTNT/Practice/project/GITHUB/Project_1/DataSet/images', filename_image)
     return filepath_image
 getFilename(filename)
 
 image_path = list(df['filepath'].apply(getFilename))
 print(image_path[:10])   # random check
 
+# 2.5 xác thực dữ liệu
+file_path = image_path[0]   # dẫn tới ảnh đầu tiên
+img = cv2.imread(file_path) # read the image
+img = io.imread(file_path) # Read the image
+fig = px.imshow(img)
+fig.update_layout(width=600, height=500, margin=dict(l=10, r=10, b=10, t=10),xaxis_title='Figure 8')
+fig.add_shape(type='rect',x0=144, x1=427, y0=119, y1=318, xref='x', yref='y',line_color='cyan')
+fig.show() # thêm để mở ảnh trong trình duyệt
