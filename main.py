4acf768fa9e9fbd2f67d83235e3544df3be60c24
minhviii <24122026@student.hcmus.edu.vn>
2024-11-22 20:11:33 +0700

    code của 2.4 nè

--- main.py
+++ main.py
@@ -18,3 +18,38 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.applications import InceptionResNetV2
 from tensorflow.keras.layers import Dense, Dropout, Flatten, Input
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
+# trich anh ra
+from google.colab import drive
+drive.mount('/content/drive', force_remount=True)
+from google.colab import drive
+drive.mount('/content/drive')
+path = glob('Database/images/*.xml')
+labels_dict = dict(filepath=[],xmin=[],xmax=[],ymin=[],ymax=[])
+for i in path:
+    info = xet.parse(i)
+    root = info.getroot()
+    member_object = root.find('object')
+    labels_info = member_object.find('bndbox')
+    xmin = int(labels_info.find('xmin').text)
+    xmax = int(labels_info.find('xmax').text)
+    ymin = int(labels_info.find('ymin').text)
+    ymax = int(labels_info.find('ymax').text)
+
+    labels_dict['filepath'].append(i)
+    labels_dict['xmin'].append(xmin)
+    labels_dict['xmax'].append(xmax)
+    labels_dict['ymin'].append(ymin)
+    labels_dict['ymax'].append(ymax)
+glob('Database/images/*.xml')
+df = pd.DataFrame(labels_dict)
+df.to_csv('labels.csv',index=False)
+df.head()
+filename = df['filepath'][0]
+def getFilename(filename):
+    filename_image = xet.parse(filename).getroot().find('filename').text
+    filepath_image = os.path.join('DataSet/images',filename_image)
+    return filepath_image
+getFilename(filename)
+image_path = list(df['filepath'].apply(getFilename))
+image_path[:10]#random check
